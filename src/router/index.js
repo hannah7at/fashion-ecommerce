e9ae1a84eb6173533f94f810b7e8e8fb3c013100
hannah7at <hanaaashraf1212@icloud.com>
2026-09-01 00:01:47 +0300

    Update design system and product data

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Home from '../views/Home.vue'
+import UserProfile from '../views/UserProfile.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/profile',
+    name: 'UserProfile',
+    component: UserProfile
   }
 ]
 
