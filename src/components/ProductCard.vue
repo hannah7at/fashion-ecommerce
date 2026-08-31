31221a8ac75b8ce5b87dc4068bea0e00fa6ea76f
Abrar Elghanam <abrarelghanam696@gmail.com>
2026-08-31 23:56:33 +0300

    Describe your changes

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card h-100">
+        <img 
+            :src="product.image"
+            :alt="product.name"
+            class="card-img-top"
+        />
+        <div class="card-body">
+            <h5 class="card-title">{{ product.name }}</h5>
+            <p class="card-text">
+                Category:{{ product.category }}
+            </p>
+            <p class="card-text fw-bold">
+                {{ product.price }}
+            </p>
+            <button class="btn btn-dark">
+                View Details
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        product:{
+            type:Object,
+            required:true
+        }
+    }
+}
+</script>
