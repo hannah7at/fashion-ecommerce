c4660f2961aa7fc51ea24e197e56f66764612f80
roqayamorad632005 <roqayamohsen854@gmail.com>
2026-09-02 20:19:42 +0300

    Add navigation guard and requiresAuth meta to /profile route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Home.vue' 
+import HomeView from '../views/HomeView.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import UserProfile from '../views/UserProfile.vue'
@@ -33,6 +33,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
 
 router.beforeEach((to, from, next) => {
   const authStore = useAuthStore()
